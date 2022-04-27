4dc7cf9285bcf6f3417570847ea51b9857fed49d
AngelRamdhan <103801832+AngelRamdhan@users.noreply.github.com>
2022-04-27 21:52:00 +0700

    Update transaction.dart

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -12,9 +12,45 @@ class Main extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+          backgroundColor: Colors.purple.shade300,
+          centerTitle: true,
+          title: Text('Transaction', style: TextStyle(fontFamily: 'Poppins')),
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.arrow_back_ios),
+          )),
+      body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(
+                top: 10,
+              ),
+              child: ListTile(
+                leading: Image.asset('assets/sepatu.jpg'),
+                trailing: Icon(Icons.call_made_rounded, color: Colors.red),
+                title: Text(
+                  "Brand New Shoes",
+                  style: TextStyle(fontFamily: 'Poppins'),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                subtitle: Text(
+                  'SH-231',
+                  style: TextStyle(fontFamily: 'Poppins'),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        backgroundColor: Colors.purple.shade300,
+        icon: Icon(Icons.add),
+        label: Text("New Transaction", style: TextStyle(fontFamily: 'Poppins')),
       ),
-      body: Center(child: Text('Ini Halaman Transaksi')),
     );
   }
 }
+
